21e3c1f9e3f862e01a96230373408d2d817f074a
JsCodexx <muhammadnomi425@gmail.cm>
2023-02-18 20:59:29 +0500

    carouselCard
    error in responsive styling

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,25 @@
 <template>
-  <ceiling-card></ceiling-card>
+  <carousel-card></carousel-card>
+  <!-- <ceiling-card></ceiling-card>
   <intro-card></intro-card>
   <privacy-card></privacy-card>
-  <ceiling-video></ceiling-video>
+  <ceiling-video></ceiling-video> -->
 </template>
 
 <script>
-import IntroCard from "@/components/IntroCard";
-import PrivacyCard from "@/components/PrivacyCard.vue";
-import CeilingCard from "./components/CeilingCard.vue";
-import CeilingVideo from "./components/CeilingVideo.vue"
+import CarouselCard from "@/components/CarouselCard"
+// import IntroCard from "@/components/IntroCard";
+// import PrivacyCard from "@/components/PrivacyCard.vue";
+// import CeilingCard from "./components/CeilingCard.vue";
+// import CeilingVideo from "./components/CeilingVideo.vue"
 export default {
   name: "App",
   components: {
-    IntroCard,
-    PrivacyCard,
-    CeilingCard,
-    CeilingVideo ,
+    // IntroCard,
+    // PrivacyCard,
+    // CeilingCard,
+    // CeilingVideo ,
+    CarouselCard,
   },
 };
 </script>
